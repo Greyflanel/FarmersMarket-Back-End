f04f3fbe3af34ae79a5b8aa5484f5214eaad22fd
Greyflanel <47545416+Greyflanel@users.noreply.github.com>
2020-06-10 00:33:06 -0500

    making some adjustments for server.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 
-const server = require('./api/server')
+const express = require("express");
+const server = express();
 const port = process.env.PORT || 3000
 
 server.listen(port, () => {
--- knexfile.js
+++ knexfile.js
@@ -1,55 +1,58 @@
-// Update with your config settings.
+require("dotenv").config();
 
 module.exports = {
+  
   development: {
-    client: "postgresql",
+    client: 'postgresql',
     connection: {
-      database: "postgres",
-      filename: "./data/produce.db",
-      user: "postgres",
-      password: "Ronald811"
-    },
+      database: 'postgres',
+      user: process.env.PG_USER,
+      password: process.env.PG_PWD,
+      filename: './data/produce.db',
+  
+    }
   },
 
   useNullAsDefault: true,
 
   migrations: {
-    directory: "./data/migrations",
+    directory: './data/migrations'
   },
 
   seeds: {
-    directory: "./data/seeds",
-  },
+    directory: './data/seeds'
+  }, 
 
   staging: {
-    client: "postgresql",
+    client: 'postgresql',
     connection: {
-      database: "my_db",
-      user: "username",
-      password: "password",
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
     },
     pool: {
       min: 2,
-      max: 10,
+      max: 10
     },
     migrations: {
-      tableName: "knex_migrations",
-    },
+      tableName: 'knex_migrations'
+    }
   },
 
   production: {
-    client: "postgresql",
+    client: 'postgresql',
     connection: {
-      database: "my_db",
-      user: "username",
-      password: "password",
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
     },
     pool: {
       min: 2,
-      max: 10,
+      max: 10
     },
     migrations: {
-      tableName: "knex_migrations",
-    },
-  },
+      tableName: 'knex_migrations'
+    }
+  }
+
 };
